6a3202ffb51a3da5abb31be50213311adace1d09
NezarianSaputra <nezarian16.siswa@smkn4bdg.sch.id>
2020-08-30 16:07:24 +0700

    Tugas PWPB Pertemuan 7

---
+++ Java/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.latihan_fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.latihan_fragment.ui.dashboard.DashboardFragment;
+import com.example.latihan_fragment.ui.home.HomeFragment;
+import com.example.latihan_fragment.ui.notifications.NotificationsFragment;
+import com.example.latihan_fragment.ui.profile.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loadFragment(new HomeFragment());
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+
+    }
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.navigation_dashboard:
+                fragment = new DashboardFragment();
+                break;
+            case R.id.navigation_notifications:
+                fragment = new NotificationsFragment();
+                break;
+            case R.id.navigation_profile:
+                fragment = new ProfileFragment();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
+
+    public boolean loadFragment (Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+}
